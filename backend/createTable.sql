4f1c8aaa3daa97b05870be97569d37528a9f0e78
anirin <atu8823atu@gmail.com>
2024-04-21 13:44:59 +0900

    backend first commit

---
+++ backend/createTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE store (
+    store_id INT PRIMARY KEY,
+    store_name VARCHAR(255),
+    address TEXT,
+    phone_number VARCHAR(20),
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
